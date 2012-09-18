04cb35a9c70c2283d1a27581e3f693c9eb3232ac
Daniel Lindstedt <lindhara@gmail.com>
2012-09-18 15:02:35 +0200

    Sid 206 Category Links done

---
+++ SportsStore/SportsStore.UnitTests/CanCreateCategories.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.Model.Entities;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CreateCategoriesTest
+    {
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+            new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+            new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+            new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+            new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            }.AsQueryable());
+
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+
+            // Act = get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0].ToString(), Apples.Name);
+            Assert.AreEqual(results[1].ToString(), Oranges.Name);
+            Assert.AreEqual(results[2].ToString(), Plums.Name);
+        }
+
+        public Category Oranges
+        {
+            get
+            { return new Category { Name = "Oranges" }; }
+        }
+        public Category Plums
+        {
+            get
+            { return new Category { Name = "Plums" }; }
+        }
+        public Category Apples
+        {
+            get
+            { return new Category { Name = "Apples" }; }
+        }
+    }
+}
--- SportsStore/SportsStore.WebUI/Global.asax.cs
+++ SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -24,18 +24,32 @@ namespace SportsStore.WebUI
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-               null, // we don't need to specify a name 
-               "Page{page}",
-               new { Controller = "Product", action = "List" }
-            ); 
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Product", action = "List", id = UrlParameter.Optional } // Parameter defaults
+            routes.MapRoute(null,
+                "", // Only matches the empty URL (i.e. /) 
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
-
-        }
+            routes.MapRoute(null,
+                "Page{page}", // Matches /Page2, /Page123, but not /PageXYZ 
+                new { controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" } // Constraints: page must be numerical 
+            );
+            routes.MapRoute(null,
+                "{category}", // Matches /Football or /AnythingWithNoSlash 
+                new { controller = "Product", action = "List", page = 1 }
+            );
+            routes.MapRoute(null,
+                "{category}/Page{page}", // Matches /Football/Page567 
+                new { controller = "Product", action = "List" }, // Defaults 
+                new { page = @"\d+" } // Constraints: page must be numerical 
+            );
+            routes.MapRoute(null, "{controller}/{action}");
+        } 
 
         protected void Application_Start()
         {
